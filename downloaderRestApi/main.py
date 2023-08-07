ba2996c27c29ad3d3c06bfcdd662f90ff6d122cf
freddyvelarde <freddyvelarde59@gmail.com>
2023-08-07 13:38:06 -0400

    Downloader code was improved.

--- downloaderRestApi/main.py
+++ downloaderRestApi/main.py
@@ -1,38 +1,15 @@
-from logging import debug
-from flask import Flask, request, jsonify, send_file
-import os
-from music_downloader import music_downloader
+from music_downloader import downloader
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return jsonify({"message": "Hello world"})
+downloader(
+    "https://www.youtube.com/playlist?list=PLJ-AcSb68R90JVHyRv7eFZBQXEDdOMm6M",
+    album_name="Mis canciones viejas",
+    song_name=None,
+)
 
 
-# Endpoint to start downloading
-#  @app.route("/download/", methods=["POST"])
-#  def start_download():
-#      video_url = request.json.get("video_url")
-#
-#      music_downloader(video_url)
-#
-#      return jsonify({"message": "Download started"})
+#  create_new_album("My Chemical Romance")
 
 
-@app.route("/download")
-def download():
-    file_path = os.path.join(
-        "/home/erick/coding/projects/mp3-downloader-python",
-        "Taylor Swift - Love Story (Live on Letterman) [mNLVMDF9mUo].mp3",
-    )
-
-    if os.path.exists(file_path):
-        return send_file(file_path, as_attachment=True)
-    else:
-        return "File not found."
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+#  music_downloader(
+#      "https://www.youtube.com/watch?v=paFCInMIPfA&ab_channel=MyChemicalRomance-Topic"
+#  )
--- downloaderRestApi/music_downloader.py
+++ downloaderRestApi/music_downloader.py
@@ -1,7 +1,46 @@
 import yt_dlp
+import os
+import shutil
+
+HOME_PATH = os.path.expanduser("~")
+MUSIC_PATH = os.path.join(HOME_PATH, "Music")
+CURRENT_PATH = os.getcwd()
 
 
-def music_downloader(video_url):
+def printing(message):
+    COLOR_YELLOW = "\033[33m"
+    COLOR_RESET = "\033[0m"
+    print(COLOR_YELLOW + message + COLOR_RESET)
+
+
+def get_all_songs():
+    content = os.listdir(CURRENT_PATH)
+    songs = []
+
+    for song in content:
+        if ".mp3" in song:
+            songs.append(song)
+    return songs
+
+
+def move_file(song_file, album_name=None):
+    song_path = f"{CURRENT_PATH}/{song_file}"
+
+    destination_path = MUSIC_PATH
+
+    if album_name is not None:
+        destination_path = f"{MUSIC_PATH}/{album_name}"
+
+    shutil.move(song_path, os.path.join(destination_path, os.path.basename(song_path)))
+
+
+def create_new_album(album_name):
+    new_album = f"{MUSIC_PATH}/{album_name}"
+    os.mkdir(new_album)
+    return new_album
+
+
+def music_downloader(video_url, output_filename=None):
     ydl_opts = {
         "format": "bestaudio/best",
         "postprocessors": [
@@ -13,5 +52,24 @@ def music_downloader(video_url):
         ],
     }
 
+    if output_filename:
+        ydl_opts["outtmpl"] = f"{output_filename}.%(ext)s"
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url])
+
+
+def downloader(video_url, album_name=None, song_name=None):
+    printing("Downloading your music.")
+    music_downloader(video_url, song_name)
+
+    printing("Getting all songs.")
+    songs = get_all_songs()
+
+    if album_name:
+        printing(f"Creating {album_name} album.")
+        create_new_album(album_name)
+
+    for song in songs:
+        move_file(song, album_name)
+    printing("Your music was downloaded.")
