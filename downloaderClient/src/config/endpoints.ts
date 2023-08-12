2437c9b57eaf7b36b369c8197c2df11eabf56d4b
freddyvelarde <freddyvelarde59@gmail.com>
2023-08-12 16:26:08 -0400

    Renaming feature added but not styles yet.

--- downloaderClient/src/config/endpoints.ts
+++ downloaderClient/src/config/endpoints.ts
@@ -5,3 +5,5 @@ export const start_download_url = `${mainUrl}/search`;
 export const download_url = `${mainUrl}/download`;
 
 export const get_all_songs = `${mainUrl}/songs`;
+
+export const rename_song = `${mainUrl}/rename`;
--- downloaderClient/src/sections/SongCards/index.tsx
+++ downloaderClient/src/sections/SongCards/index.tsx
@@ -1,20 +1,70 @@
+import { FormEvent, useState } from "react";
 import useDownloader from "../../hooks/useDownloader";
 import downloadIcon from "../../assets/download-icon.svg";
 import "./song.styles.css";
+import { rename_song } from "../../config/endpoints";
 
 export default function SongCards() {
-  const { downloadSong, songList } = useDownloader();
+  const { downloadSong, songList, getAllSongs } = useDownloader();
+  const [renamingIndex, setRenamingIndex] = useState(-1);
+  const [editedLink, setEditedLink] = useState("");
+  const [oldSongName, setOldSongName] = useState("");
+
+  const renameSong = async () => {
+    await fetch(rename_song, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        song_name: oldSongName,
+        new_song_name: editedLink,
+      }),
+    });
+    getAllSongs();
+  };
 
   const eventDownloadSong = (link: string) => {
     downloadSong(link);
   };
 
+  const startRename = (index: number, song: string) => {
+    setRenamingIndex(index);
+    setEditedLink(songList[index]);
+    setOldSongName(song);
+  };
+
+  const submitEventHandler = (e: FormEvent) => {
+    e.preventDefault();
+    renameSong();
+    setRenamingIndex(-1);
+  };
+
   return (
     <div>
       {songList.length >= 1 ? (
-        songList.map((link: string, index: number) => (
+        songList.map((link, index) => (
           <div className="card" key={index}>
-            <span>{link}</span>
+            {renamingIndex === index ? (
+              <div>
+                <form onSubmit={submitEventHandler}>
+                  <input
+                    type="text"
+                    value={editedLink}
+                    onChange={(e) => setEditedLink(e.target.value)}
+                  />
+                  <button>rename</button>
+                </form>
+                <span onClick={() => setRenamingIndex(-1)} id="title-song">
+                  x
+                </span>
+              </div>
+            ) : (
+              <span id="title-song" onClick={() => startRename(index, link)}>
+                {link}
+              </span>
+            )}
+
             <img
               onClick={() => eventDownloadSong(link)}
               src={downloadIcon}
