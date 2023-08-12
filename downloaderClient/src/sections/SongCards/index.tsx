d07c312f57b48d342244e745282b6ab4d4c83e18
freddyvelarde <freddyvelarde59@gmail.com>
2023-08-12 17:03:40 -0400

    Styles added.

--- downloaderClient/src/sections/SongCards/index.tsx
+++ downloaderClient/src/sections/SongCards/index.tsx
@@ -1,43 +1,46 @@
-import { FormEvent, useState } from "react";
+import { useState, FormEvent } from "react";
 import useDownloader from "../../hooks/useDownloader";
 import downloadIcon from "../../assets/download-icon.svg";
 import "./song.styles.css";
 import { rename_song } from "../../config/endpoints";
 
-export default function SongCards() {
+export default function SongCard() {
   const { downloadSong, songList, getAllSongs } = useDownloader();
-  const [renamingIndex, setRenamingIndex] = useState(-1);
+  const [renamingIndex, setRenamingIndex] = useState<number | null>(null);
   const [editedLink, setEditedLink] = useState("");
   const [oldSongName, setOldSongName] = useState("");
 
   const renameSong = async () => {
-    await fetch(rename_song, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        song_name: oldSongName,
-        new_song_name: editedLink,
-      }),
-    });
-    getAllSongs();
+    try {
+      await fetch(rename_song, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          song_name: oldSongName,
+          new_song_name: editedLink,
+        }),
+      });
+      getAllSongs();
+    } catch (error) {
+      console.error("Error renaming song:", error);
+    }
   };
 
-  const eventDownloadSong = (link: string) => {
+  const handleDownload = (link: string) => {
     downloadSong(link);
   };
 
-  const startRename = (index: number, song: string) => {
+  const handleRenameStart = (index: number, song: string) => {
     setRenamingIndex(index);
     setEditedLink(songList[index]);
     setOldSongName(song);
   };
 
-  const submitEventHandler = (e: FormEvent) => {
-    e.preventDefault();
+  const eventRenameSubmit = () => {
     renameSong();
-    setRenamingIndex(-1);
+    setRenamingIndex(null);
   };
 
   return (
@@ -46,27 +49,35 @@ export default function SongCards() {
         songList.map((link, index) => (
           <div className="card" key={index}>
             {renamingIndex === index ? (
-              <div>
-                <form onSubmit={submitEventHandler}>
-                  <input
-                    type="text"
-                    value={editedLink}
-                    onChange={(e) => setEditedLink(e.target.value)}
-                  />
-                  <button>rename</button>
-                </form>
-                <span onClick={() => setRenamingIndex(-1)} id="title-song">
-                  x
-                </span>
+              <div id="renaming-form">
+                <input
+                  type="text"
+                  value={editedLink}
+                  onChange={(e) => setEditedLink(e.target.value)}
+                />
+                <div id="buttons">
+                  <button type="submit" onClick={eventRenameSubmit}>
+                    rename
+                  </button>
+                  <button
+                    onClick={() => setRenamingIndex(null)}
+                    id="title-song"
+                  >
+                    x
+                  </button>
+                </div>
               </div>
             ) : (
-              <span id="title-song" onClick={() => startRename(index, link)}>
+              <span
+                id="title-song"
+                onClick={() => handleRenameStart(index, link)}
+              >
                 {link}
               </span>
             )}
 
             <img
-              onClick={() => eventDownloadSong(link)}
+              onClick={() => handleDownload(link)}
               src={downloadIcon}
               alt="download-icon"
               width={20}
